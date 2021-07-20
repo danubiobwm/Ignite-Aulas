087fe4a081432b17e8c57da9f6d081c0bf57d747
danubiobwm <danubio.bwm@gmail.com>
2021-07-19 23:46:01 -0300

    feat: :arrow_up: Inserido Tipagem e Uuid
    Inserido Tipagem e Uuid
    
    Inserido Tipagem e Uuid

--- rentalx/src/routes/Categories.routes.ts
+++ rentalx/src/routes/Categories.routes.ts
@@ -1,17 +1,25 @@
 import { Router } from 'express';
+import { Category } from 'model/Category';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
-const categories=[];
+const categories: Category[] = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
-  const { name, description  } = request.body;
-  categories.push({
-    name, description
-  });
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date()
+  })
+
+  categories.push(category);
+
   return response.status(201).json(categories);
 });
-
-
 
 export { categoriesRoutes };
