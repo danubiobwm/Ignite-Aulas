be388d7302cb840334bfcaab9cdec91c6d8034f2
danubiobwm <danubio.bwm@gmail.com>
2021-10-10 20:54:00 -0300

    Avatar Users

--- rentalx/src/routes/users.routes.ts
+++ rentalx/src/routes/users.routes.ts
@@ -1,10 +1,26 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
-import {Router} from 'express';
+import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
+import { ensureAuthenticated } from 'middlewares/ensureAuthenticated';
+
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig);
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
 
-export {usersRoutes}
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle,
+);
+
+export { usersRoutes };
